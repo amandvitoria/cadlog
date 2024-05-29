d1e55b612bfeac0b354424e90a5b90e5f7415bbb
amandvitoria <amandvitoria244@gmail.com>
2024-05-29 10:11:03 -0300

    criação de estrutura para login

--- login.php
+++ login.php
@@ -7,10 +7,10 @@
     <meta name="description" content="">
     <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
     <meta name="generator" content="Hugo 0.122.0">
-    <title>login</title>
-
+    <title>pagina</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
 
     <link rel="canonical" href="https://getbootstrap.com/docs/5.3/examples/cover/">
 
@@ -21,7 +21,7 @@
 <link href="../assets/dist/css/bootstrap.min.css" rel="stylesheet">
 
     <style>
-      .bd-placeholder-img{
+      .bd-placeholder-img {
         font-size: 1.125rem;
         text-anchor: middle;
         -webkit-user-select: none;
@@ -97,7 +97,7 @@
       .bd-mode-toggle .dropdown-menu .active .bi {
         display: block !important;
       }
-      
+
     </style>
 
     
@@ -162,21 +162,26 @@
     <div>
       <h3 class="float-md-start mb-0">Cover</h3>
       <nav class="nav nav-masthead justify-content-center float-md-end">
-        <a class="nav-link fw-bold py-1 px-0 " aria-current="page" href="pagina.php">Home</a>
-        <a class="nav-link fw-bold py-1 px-0" href="cad.php">Features</a>
-        <a class="nav-link fw-bold py-1 px-0 active  " href="login.php">Contact</a>
+        <a class="nav-link fw-bold py-1 px-0 active " aria-current="page" href="pagina.php">Inicio</a>
+        <a class="nav-link fw-bold py-1 px-0" href="cad.php">Cadastro</a>
+        <a class="nav-link fw-bold py-1 px-0" href="login.php">Login</a>
       </nav>
     </div>
   </header>
 
   <main class="px-3">
-    <h1>Cover your page.</h1>
-    <p class="lead">Cover is a one-page template for building simple and beautiful home pages. Download, edit the text, and add your own fullscreen background photo to make it your own.</p>
-    <p class="lead">
-      <a href="#" class="btn btn-lg btn-light fw-bold border-white bg-white">Learn more</a>
-    </p>
-  </main>
+   <form method="get" action="">
 
+   <label for="email">email</label>
+   <input type="email">
+
+   <label for="password">senha</label>
+   <input type="password">
+
+   <button type="submit">Login</button>
+
+   </form>
+  </main>
   <footer class="mt-auto text-white-50">
     <p>Cover template for <a href="https://getbootstrap.com/" class="text-white">Bootstrap</a>, by <a href="https://twitter.com/mdo" class="text-white">@mdo</a>.</p>
   </footer>
